548d6f505c1662c5b279fb0de6ee97718dbc9c48
lazybensch <lazybensch@gmail.com>
2016-06-06 11:54:55 +0200

    asdf

--- addon/components/resolve-content.js
+++ addon/components/resolve-content.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/resolve-content';
-const { A, Component, computed, run: { schedule }, RSVP: { Promise } } = Ember;
+const { A, Component, run: { schedule }, RSVP: { Promise } } = Ember;
 
 export default Component.extend({
   layout,
@@ -9,83 +9,53 @@ export default Component.extend({
 
   init() {
 
-    this.set('isLoading', true);
     this.set('sanitizedContent', A());
     schedule('afterRender', this, () => {
 
-      debugger;
       const content = this.get('content');
       if (!content) {
         throw 'content must be set for generic-input-field';
       }
-      debugger;
       const sanitizedContent = this.get('sanitizedContent');
       const isPlainArray = typeof content.length === 'number';
       const isPlainPromiseArray = content[0] && content[0].constructor === Promise;
       const isPromise = content.constructor === Promise;
 
-      debugger;
-
-      console.log('foo');
       const pushItems = (content) => {
         const isPromise = content.get('firstObject.constructor') === Promise;
-        console.log('ASDF',isPromise);
         if (isPromise) {
-          Promise.all(content).then((rc) => {
-            sanitizedContent.addObjects(rc)
-            this.set('isLoading', false);
-          });
+          Promise.all(content).then((rc) => sanitizedContent.addObjects(rc));
         } else {
           sanitizedContent.addObjects(content);
-          this.set('isLoading', false);
         }
       };
 
       if (isPlainArray && !isPlainPromiseArray){
-        console.log('isPlainArray && !isPlainPromiseArray){',content)
         sanitizedContent.addObjects(content);
-        this.set('isLoading', false);
-        this._super();
         return;
       }
 
-      console.log(isPlainArray, isPlainPromiseArray);
       if (isPlainArray && isPlainPromiseArray){
-        console.log('isPlainArray && isPlainPromiseArray){')
-        Promise.all(content).then((rc) => {
-          sanitizedContent.addObjects(rc)
-          this.set('isLoading', false);
-        });
-        this._super();
+        Promise.all(content).then((rc) => sanitizedContent.addObjects(rc));
         return;
       }
 
       if (isPromise){
-        console.log('isPromise){')
-        content.then((x) => {
-          sanitizedContent.addObjects(x);
-          this.set('isLoading', false);
-        });
-        this._super();
+        content.then((x) => { sanitizedContent.addObjects(x); });
         return;
       }
 
       if (!isPromise && content.isLoaded) {
-        console.log('!isPromise && content.isLoaded) {',content)
         pushItems(content);
-        this._super();
         return;
       }
 
       if (!isPromise && !content.isLoaded) {
-        console.log('!isPromise && !content.isLoaded) {',content)
         content.then(pushItems);
-        this._super();
         return;
       }
     });
 
-    this.set('isLoading', false);
     this._super();
   }
 
