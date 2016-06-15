d16667cf3f2acd875053233ede1f22c538c6cfc5
Rupert Klopper <rupert.klopper@liquidm.com>
2016-06-15 17:54:11 +0200

    Add better tests for root-component

--- tests/integration/components/root-component-test.js
+++ tests/integration/components/root-component-test.js
@@ -1,3 +1,5 @@
+import Ember from 'ember';
+const { A, $ } = Ember;
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -5,20 +7,54 @@ moduleForComponent('root-component', 'Integration | Component | root component',
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders with no selections', function(assert) {
 
-  this.render(hbs`{{root-component}}`);
+  this.set('all', A([
+    { id: '1', label: 'A' },
+    { id: '2', label: 'B' },
+    { id: '3', label: 'C' }
+  ]));
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('input', A([]));
 
-  // Template block usage:
-  this.render(hbs`
-    {{#root-component}}
-      template block text
-    {{/root-component}}
-  `);
+  this.render(hbs`{{root-component all=all input=input}}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  const genericFields = this.$('div.generic-field');
+  const parentLevel = genericFields[0];
+  const childLevel = genericFields[1];
+
+  const labels = this.$('div.content label');
+
+  assert.equal(labels.length, 3, 'Rendered 3 labels');
+
+});
+
+test('it renders with one selection', function(assert) {
+
+  this.set('all', A([
+    { id: '1', label: 'A' },
+    { id: '2', label: 'B' },
+    { id: '3', label: 'C' }
+  ]));
+
+  this.set('input', A([
+    { id: '1', label: 'A' },
+  ]));
+
+  this.render(hbs`{{root-component all=all input=input}}`);
+
+  const genericFields = this.$('div.generic-field');
+  const parentLevel = $(genericFields[0]);
+  const childLevel = $(genericFields[1]);
+
+  const labels = parentLevel.find('div.content > label');
+  const notSelected = labels.filter((x, label) => {
+    return label.innerText !== '[]';
+  });
+  const selected = childLevel.find('label.collapsed');
+
+  assert.equal(notSelected.length, 2, 'Rendered 2 un-selected labels');
+
+  assert.equal(selected.length, 1, 'Rendered 1 selected label');
+
 });
